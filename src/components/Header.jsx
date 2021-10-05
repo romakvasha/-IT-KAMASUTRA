a727818cc8de93a6854672d9215958a9e08f1a32
Roman Kvasha <romajan.ru@gmail.com>
2021-10-05 19:51:10 +0200

    13 Urok

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://1757140519.rsc.cdn77.org/blog/wp-content/uploads/2018/05/1-google-logo.png'></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://img.hospital/wp-content/uploads/2020/02/ARS-02.png'></img>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                New post
+                </div>
+            </div>
+            <div>
+                <div>
+                post1
+                </div>
+                <div>
+                post2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
