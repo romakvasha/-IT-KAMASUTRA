341dd0547cfb44029cfec841bedea9d25fb28196
Roman Kvasha <romajan.ru@gmail.com>
2021-10-10 12:37:08 +0200

    Add dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>Руслана</div>
+        <div className={s.dialog}>Рома</div>
+        <div className={s.dialog}>Тарас</div>
+        <div className={s.dialog}>Сірожа</div>
+        <div className={s.dialog}>Андрій</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Привіт</div>
+        <div className={s.message}>Я вивчаю програмування</div>
+        <div className={s.message}>Привіт! як справи?</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
