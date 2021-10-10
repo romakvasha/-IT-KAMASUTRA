3c18204fa7d9d3949dd791929c9294fcbbfefdfb
Roman Kvasha <romajan.ru@gmail.com>
2021-10-10 20:25:59 +0200

    (IK-23) Refactor Dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import s from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DIALOGS = "/dialogs";
+
+const dialogItems = ["Руслана", "Роман", "Тарас", "Сірожа", "Андрій"];
+
+const DialogItem = (props) => {
+  return (
+    <div className={s.dialog}>
+      <NavLink to={`${DIALOGS}/${props.id}`} activeClassName={s.active}>
+        {props.name}
+      </NavLink>
+    </div>
+  );
+};
+
+const message = (props) => {
+  return (
+    <div className={s.messages}>
+      <NavLink to={`/${DIALOGS}/${props.id}`} activeClassName={s.active}>
+        {props.name}
+      </NavLink>
+    </div>
+  );
+};
 
 const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <div className={s.dialog}>Руслана</div>
-        <div className={s.dialog}>Рома</div>
-        <div className={s.dialog}>Тарас</div>
-        <div className={s.dialog}>Сірожа</div>
-        <div className={s.dialog}>Андрій</div>
+        {dialogItems.map((el, index) => (
+          <DialogItem name={el} id={index + 1} key={`DialogItem_${index}`} />
+        ))}
       </div>
       <div className={s.messages}>
         <div className={s.message}>Привіт</div>
