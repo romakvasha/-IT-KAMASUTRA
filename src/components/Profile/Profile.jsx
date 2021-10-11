86ed558800b074115c118581d4509f2554195299
Roman Kvasha <romajan.ru@gmail.com>
2021-10-12 01:20:49 +0200

    Добавив функцію МАП

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import MyPosts from "./myPosts/MyPosts";
 import s from "./Profile.module.css";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
   return (
     <div>
-      <div>
-        <img
-          className={s.img}
-          src="https://images.unsplash.com/photo-1633684268648-c6360f92a08d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80"
-        ></img>
-      </div>
-      <div>ava + description</div>
+      <ProfileInfo />
       <MyPosts />
     </div>
   );
--- src/components/Profile/myPosts/MyPosts.jsx
+++ src/components/Profile/myPosts/MyPosts.jsx
@@ -3,20 +3,30 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+  let postData = [
+    { id: 1, message: "Привіт, як справи?", likesCount: 12 },
+    { id: 2, message: "Це мій перший пост", likesCount: 20 },
+    { id: 3, message: "Це мій другий пост", likesCount: 30 },
+  ];
+
+  let MyPostsElement = postData.map((p) => (
+    <Post message={p.message} id={p.id} likesCount={p.likesCount} />
+  ));
+
   return (
-    <div>
+    <div className={s.postsBlog}>
       <div>
-        My post
+        <h3>My post</h3>
         <div>
-          <textarea></textarea>
-          <button>Add post</button>
+          <div>
+            <textarea></textarea>
+          </div>
+          <div>
+            <button>Add post</button>
+          </div>
         </div>
       </div>
-      <div className={s.posts}>
-        <Post message="Привіт, як справи?" like="15" />
-        <Post message="Це мій перший пост" like="20" />
-        <Post message="Це мій другий пост" like="30" />
-      </div>
+      <div className={s.posts}>{MyPostsElement}</div>
     </div>
   );
 };
