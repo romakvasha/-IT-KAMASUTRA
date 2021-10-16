efc7ddb074d0177e9ccac39ed41c613980808f90
Roman Kvasha <romajan.ru@gmail.com>
2021-10-16 21:23:45 +0200

    (US-3) update image height

--- src/components/NavBar/FriendsBar/FriendsBar.jsx
+++ src/components/NavBar/FriendsBar/FriendsBar.jsx
@@ -5,7 +5,8 @@ import { NavLink } from "react-router-dom";
 const FriendsBar = () => {
   return (
     <div className={s.friendsBar}>
-      <h5>Friends</h5>
+      <h5>Friends 1</h5>
+      <p>12123</p>
     </div>
   );
 };
