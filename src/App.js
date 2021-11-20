9b0b046256b48f349b84df5683ca00388f775985
Roman Kvasha <romajan.ru@gmail.com>
2021-11-21 00:59:05 +0100

    cookie, login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.css";
 import Nav from "./components/NavBar/Nav";
-import Header from "./components/Header/Header";
 import { Route } from "react-router-dom";
 import { DIALOGS, FINDUSERS, PROFILE } from "./components/NavBar/constats";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = (props) => {
   return (
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Nav />
       <div class="app-wrapper-content">
         <Route path={DIALOGS} render={() => <DialogsContainer />} />
---
+++ src/redux/auth-reducer.js
@@ -0,0 +1,28 @@
+const SET_USER_DATA = "SET_USER_DATA";
+
+const initialState = {
+  userId: null,
+  login: null,
+  email: null,
+  isAuth: false,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER_DATA:
+      return {
+        ...state,
+        ...action.data,
+        isAuth: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export const seAuthtUsersdata = (userId, login, email) => ({
+  type: SET_USER_DATA,
+  data: { userId, login, email },
+});
+
+export default authReducer;
