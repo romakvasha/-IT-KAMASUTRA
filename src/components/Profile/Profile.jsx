74eb75530f5deeb765854dd8e0ca8fdb5958dc6a
Roman Kvasha <romajan.ru@gmail.com>
2022-03-13 09:54:08 +0100

    загрузка изображения

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import MyPostsContainer from "./myPosts/MyPostsContainer";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
-const Profile = ({ profile, status, updateStatus }) => {
+const Profile = ({ profile, status, updateStatus, isOwner, savePhoto }) => {
   return (
     <div>
       <ProfileInfo
         profile={profile}
         status={status}
         updateStatus={updateStatus}
+        isOwner={isOwner}
+        savePhoto={savePhoto}
       />
       <MyPostsContainer />
     </div>
